b8d8192af9a50b597591e6e6ca69eafe2edd5fda
roryg11 <Rory.C.Grant@gmail.com>
2014-12-08 10:32:03 -0800

    Users can see all data about a product on show page

---
+++ db/migrate/20141208182116_add_timestamp_to_products.rb
@@ -0,0 +1,7 @@
+class AddTimestampToProducts < ActiveRecord::Migration
+  def change
+    change_table :products do |t|
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208180949) do
+ActiveRecord::Schema.define(version: 20141208182116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: true do |t|
-    t.string  "name",        null: false
-    t.string  "description"
-    t.integer "price"
+    t.string   "name",        null: false
+    t.string   "description"
+    t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
