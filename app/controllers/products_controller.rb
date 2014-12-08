d46c73d82c8ff02a92e6bf61bc99248f731de372
roryg11 <Rory.C.Grant@gmail.com>
2014-12-08 10:20:10 -0800

    creating a product redirects to show page which shows success alert messag

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,14 @@ class ProductsController < ApplicationController
 
     if @product.save
       redirect_to product_path(@product)
+      flash[:notice] = "Product was created successfully"
     else
       render :new
     end
+  end
+
+  def show
+    @product=Product.find(params[:id])
   end
 
   private
